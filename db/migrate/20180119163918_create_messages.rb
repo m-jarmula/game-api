424bb088ea8376b33ab4767f5d1f4b39595ff0af
Maciej Jarmula <maciej.jarmula@onthebeach.co.uk>
2018-01-19 17:26:20 +0000

    Add Chat channel and messages model

---
+++ db/migrate/20180119163918_create_messages.rb
@@ -0,0 +1,8 @@
+class CreateMessages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages do |t|
+      t.references :player, foreign_key: true
+      t.text :message
+    end
+  end
+end
